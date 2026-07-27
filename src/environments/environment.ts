b91e3e155dd88ac6b60e795aa0975c4a46ed75be
Jose Gabriel Cortazar Ocampo <josegcort@gmail.com>
2026-07-27 09:16:53 -0500

    feat: add Firebase configuration files for hosting and environment setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBmHUAu0Nl6vPfFkZc6yZl97NRaaloTX7Y',
+    authDomain: 'serena-axm.firebaseapp.com',
+    projectId: 'serena-axm',
+    storageBucket: 'serena-axm.firebasestorage.app',
+    messagingSenderId: '520274214625',
+    appId: '1:520274214625:web:023afd8be752c7524781a0',
+  },
+};
